6b2832462215d649f84157370b48620816828e46
f7deat <tan.dct9x@gmail.com>
2025-01-07 11:07:23 +0700

    add epplus

---
+++ Helpers/ExcelHelper.cs
@@ -0,0 +1,20 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace THPCore.Helpers;
+
+public class ExcelHelper
+{
+    public const string ExcelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    /// <summary>
+    /// Apply borders to the given range of cells
+    /// </summary>
+    /// <param name="cells"></param>
+    public static void ApplyBordersToRange(ExcelRange cells)
+    {
+        cells.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+        cells.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+        cells.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+        cells.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+    }
+}
--- Models/THPResult.cs
+++ Models/THPResult.cs
@@ -36,3 +36,40 @@ public class THPResult
 
     public static THPResult Ok(object? data) => new() { _data = data, Succeeded = true };
 }
+
+public class THPResult<T>
+{
+    private static readonly THPResult<T> _success = new() { Succeeded = true };
+    private string _message = string.Empty;
+    private T? _data;
+    /// <summary>
+    /// Flag indicating whether if the operation succeeded or not.
+    /// </summary>
+    /// <value>True if the operation succeeded, otherwise false.</value>
+    public bool Succeeded { get; protected set; }
+    public string Message => _message;
+    public T? Data => _data;
+
+    /// <summary>
+    /// Returns an <see cref="THPResult"/> indicating a successful identity operation.
+    /// </summary>
+    /// <returns>An <see cref="THPResult"/> indicating a successful operation.</returns>
+    public static THPResult<T> Success => _success;
+
+    /// <summary>
+    /// Creates an <see cref="THPResult"/> indicating a failed identity operation, with a list of <paramref name="message"/> if applicable.
+    /// </summary>
+    /// <param name="message">An optional array of <see cref="Message"/>s which caused the operation to fail.</param>
+    /// <returns>An <see cref="THPResult"/> indicating a failed identity operation, with a list of <paramref name="message"/> if applicable.</returns>
+    public static THPResult<T> Failed(string message)
+    {
+        var result = new THPResult<T> { Succeeded = false };
+        if (!string.IsNullOrEmpty(message))
+        {
+            result._message = message;
+        }
+        return result;
+    }
+
+    public static THPResult<T> Ok(T? data) => new() { _data = data, Succeeded = true };
+}
