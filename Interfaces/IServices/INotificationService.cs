347eb112986864b2717998dcb19fb7e25e9be00c
f7deat <tan.dct9x@gmail.com>
2025-04-24 16:53:36 +0700

    Update notification service and version to 1.0.6
    - Enhanced `.gitignore` to exclude additional files.
    - Added `INotificationService` interface with methods for creating notifications.
    - Implemented `NotificationService` class to handle notification logic.
    - Updated `README.md` for NuGet package version change to 1.0.6.
    - Changed project version in `THPCore.csproj` to 1.0.6.

---
+++ Interfaces/IServices/INotificationService.cs
@@ -0,0 +1,7 @@
+﻿namespace THPCore.Interfaces.IServices;
+
+public interface INotificationService
+{
+    Task CreateAsync(string title, string content, IEnumerable<string> recipients);
+    Task CreateAsync(string title, string content, string recipient);
+}
